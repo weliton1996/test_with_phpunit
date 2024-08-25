32d3a08407b618a4e3fe9bfebf375137c0f38efa
weliton1996 <welitonkira@gmail.com>
2024-08-24 22:30:24 -0300

    teste customer

---
+++ with_phpunit/src/OrderBundle/Test/Entity/CustomerTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace OrderBundle\Test\Entity;
+
+use OrderBundle\Entity\Customer;
+use PHPUnit\Framework\TestCase;
+
+class CustomerTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider customerAllowedDataProvider
+     */
+    public function isAllowedToOrder($isActive, $isBlocked, $expectedAlloed)
+    {
+        $customer = new Customer(
+            $isActive,
+            $isBlocked,
+            'Weliton Silva',
+            '+5599988880505'
+        );
+
+        $isAllowed = $customer->isAllowedToOrder();
+
+        $this->assertEquals($expectedAlloed, $isAllowed);
+    }
+
+    public function customerAllowedDataProvider()
+    {
+        return [
+            'shouldBeAllowedWhenIsActiveAndNotBlocked' => [
+                'isActive' => true,
+                'isBlocked' => false,
+                'expectedAllowed' => true
+            ],
+            'shouldNotBeAllowedWhenIsActiveButIsBlocked' => [
+                'isActive' => true,
+                'isBlocked' => true,
+                'expectedAllowed' => false
+            ],
+            'shouldNotBeAllowedWhenIsNotActive' => [
+                'isActive' => false,
+                'isBlocked' => false,
+                'expectedAllowed' => false
+            ],
+            'shouldNotBeAllowedWhenIsNotActiveAndIsBlocked' => [
+                'isActive' => false,
+                'isBlocked' => true,
+                'expectedAllowed' => false
+            ]
+        ];
+    }
+}
