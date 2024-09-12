8895f2b761df53cac15c943f71ccf78afde4c59a
weliton1996 <welitonkira@gmail.com>
2024-09-11 21:38:32 -0300

    Praticando o ciclo Red Green Refactor

--- with_phpunit/src/OrderBundle/Service/CustomerCategoryService.php
+++ with_phpunit/src/OrderBundle/Service/CustomerCategoryService.php
@@ -6,24 +6,31 @@ use OrderBundle\Entity\Customer;
 
 class CustomerCategoryService
 {
+    // const CATEGORY_NEW_USER = 'new-user';
+    // const CATEGORY_LIGHT_USER = 'light-user';
+    // const CATEGORY_MEDIUM_USER = 'medium-user';
+    // const CATEGORY_HEAVY_USER = 'heavy-user';
+
+    // private $categories;
+
+    // public function addCategory(CustomerCategoryInterface $category)
+    // {
+    //     $this->categories[] = $category;
+    // }
+
+    // public function getUsageCategory(Customer $customer)
+    // {
+    //     foreach ($this->categories as $category) {
+    //         if ($category->isEligible($customer)) {
+    //             return $category->getCategoryName();
+    //         }
+    //     }
+    // }
+
     const CATEGORY_NEW_USER = 'new-user';
-    const CATEGORY_LIGHT_USER = 'light-user';
-    const CATEGORY_MEDIUM_USER = 'medium-user';
-    const CATEGORY_HEAVY_USER = 'heavy-user';
-
-    private $categories;
-
-    public function addCategory(CustomerCategoryInterface $category)
-    {
-        $this->categories[] = $category;
-    }
 
     public function getUsageCategory(Customer $customer)
     {
-        foreach ($this->categories as $category) {
-            if ($category->isEligible($customer)) {
-                return $category->getCategoryName();
-            }
-        }
+        return self::CATEGORY_NEW_USER;
     }
 }
---
+++ with_phpunit/src/OrderBundle/Test/Service/CustomerCategoryServiceTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace OrderBundle\Test\Service;
+use OrderBundle\Entity\Customer;
+use OrderBundle\Service\CustomerCategoryService;
+use PHPUnit\Framework\TestCase;
+
+class CustomerCategoryServiceTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function customerShouldBeNewUser()
+    {
+        $customerCategoryService = new CustomerCategoryService();
+
+        $customer = new Customer();
+        $usageCategory = $customerCategoryService->getUsageCategory($customer);
+
+        $this->assertEquals(CustomerCategoryService::CATEGORY_NEW_USER, $usageCategory);
+    }
+}
